a423f56df5531411cf42a164416f52c1e95dce52
hi-i-m-gtooth <noface21105@gmail.com>
2023-11-23 02:07:05 +0800

    fix: typo -- yzliu

--- run_post.sh
+++ run_post.sh
@@ -1,9 +1,9 @@
-answer='submissions/test_2ep/answer.txt'
-rlt_dir='submissions/test_2ep_post'
+answer='submissions/test_150ep/answer.txt'
+rlt_dir='submissions/test_150ep_post'
 file_dirs='raw_data/valid/dataset'
 # check if rlt exists, if not, create it
-if [ ! -d $rlt ]; then
-    mkdir -p $rlt
+if [ ! -d $rlt_dir ]; then
+    mkdir -p $rlt_dir
 fi
 
 python gpostprocess.py -a $answer -r $rlt_dir/answer.txt -fd $file_dirs
