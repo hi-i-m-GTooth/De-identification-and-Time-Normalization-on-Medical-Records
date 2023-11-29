90f34aacae141724f38d2bc876e8515a250e4d1c
hi-i-m-gtooth <noface21105@gmail.com>
2023-11-30 05:06:56 +0800

    feat: add new param -- yzliu

--- experiments/run_train.sh
+++ experiments/run_train.sh
@@ -1,11 +1,11 @@
 cur_dir_path=$(cd "$(dirname "$0")"; pwd)
 
-epoch=150
-exp_name=new_split_prefix_real_410m_${epoch}ep
+epoch=20
+exp_name='410m_20ep'
 save_epoch=1 # 0 means only save final model
 
-train_file=train_prefix.gsv
-valid_file=valid_prefix.gsv
+train_file=train.gsv
+valid_file=valid.gsv
 
 model=EleutherAI/pythia-410m
 revision=step3000
@@ -13,4 +13,5 @@ revision=step3000
 python3 $cur_dir_path/../train.py -n $exp_name --subdataset_size -1 -b 4 -e $epoch \
                             --train_file $train_file --valid_file $valid_file \
                             --model_name $model --revision $revision \
-                            --lr 3e-5 -se $save_epoch --device cuda:0
+                            --lr 3e-5 -se $save_epoch --device cuda:0 \
+                            --use_scheduler --use_log
